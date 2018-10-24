14d4962351f10a728109481f7f990a3046f906d5
Ali Rizvi <frontear4594@gmail.com>
2018-10-23 21:20:22 -0400

    Fix a null reference error
    Whoops

--- src/main/java/com/frontear/hephaestus/Hephaestus.java
+++ src/main/java/com/frontear/hephaestus/Hephaestus.java
@@ -18,17 +18,17 @@ public class Hephaestus
 {
     public static HephaestusClient client;
     public static HephaestusCommand command;
+    public static CommandArgs[] commandArgs;
 
     @EventHandler
-    public void postInit(FMLPostInitializationEvent event)
-    {
-        CommandArgs[] commandArgs = new CommandArgs[] {
+    public void postInit(FMLPostInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(client = new HephaestusClient());
+
+        commandArgs = new CommandArgs[] {
                 new CommandArgs("help", "usage: list all commands"),
                 new CommandArgs("bind", "usage: bind <module> [key]"),
                 new CommandArgs("version","usage: display " + client.CLIENT_NAME.toLowerCase() + " version")
         };
-        
-        MinecraftForge.EVENT_BUS.register(client = new HephaestusClient());
         ClientCommandHandler.instance.registerCommand(command = new HephaestusCommand("hephaestus", "Usage: " + " /hephaestus " + " <command> ", commandArgs));
     }
 }
