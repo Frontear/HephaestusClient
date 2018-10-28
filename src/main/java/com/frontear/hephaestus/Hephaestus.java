dd59de2c7f016457e5bd6edf67070c066aa1650c
Ali Rizvi <frontear4594@gmail.com>
2018-10-28 18:00:45 -0400

    Remove all hints of command
    It sucks, frankly. We'll make it better

--- src/main/java/com/frontear/hephaestus/Hephaestus.java
+++ src/main/java/com/frontear/hephaestus/Hephaestus.java
@@ -1,9 +1,6 @@
 package com.frontear.hephaestus;
 
 import com.frontear.hephaestus.client.HephaestusClient;
-import com.frontear.hephaestus.client.HephaestusCommand;
-import com.frontear.hephaestus.helpers.CommandArgs;
-import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -17,18 +14,9 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 public class Hephaestus
 {
     public static HephaestusClient client;
-    public static HephaestusCommand command;
-    public static CommandArgs[] commandArgs;
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(client = new HephaestusClient());
-
-        commandArgs = new CommandArgs[] {
-                new CommandArgs("help", "usage: list all commands"),
-                new CommandArgs("bind", "usage: bind <module> [key]"),
-                new CommandArgs("version","usage: display " + client.CLIENT_NAME.toLowerCase() + " version")
-        };
-        ClientCommandHandler.instance.registerCommand(command = new HephaestusCommand("hephaestus", "Usage: " + " /hephaestus " + " <command> ", commandArgs));
     }
 }
