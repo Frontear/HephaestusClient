f8bf7e316fe793a9a2a87c0e6e3c804befd0bf43
Ali Rizvi <frontear4594@gmail.com>
2018-10-27 21:58:03 -0400

    something something, smaller code something

--- src/main/java/com/frontear/hephaestus/managers/GUIManager.java
+++ src/main/java/com/frontear/hephaestus/managers/GUIManager.java
@@ -1,26 +1,26 @@
 package com.frontear.hephaestus.managers;
 
 import com.frontear.hephaestus.Hephaestus;
-import com.frontear.hephaestus.helpers.UIPosition;
+import com.frontear.hephaestus.modules.api.Module;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
 
 import java.awt.*;
-import java.io.IOException;
+import java.util.ArrayList;
 
 public class GUIManager extends GuiScreen {
-    private UIPosition uiPosition;
     private final float scaleFactor = 1.5f;
-    public GUIManager() {
-        uiPosition = new UIPosition();
-    }
+    private final int x_pos = 2;
+
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         drawDefaultBackground();
-        uiPosition.GLScale(scaleFactor);
-        drawRect(2, 2, 80, mc.fontRendererObj.FONT_HEIGHT * 2, new Color(128, 128, 128).getRGB());
+        GlStateManager.scale(scaleFactor, scaleFactor, scaleFactor);
+        drawRect(x_pos, 2, 80, mc.fontRendererObj.FONT_HEIGHT * 2, new Color(128, 128, 128).getRGB());
         mc.fontRendererObj.drawString("Hacks", 25, (4 + mc.fontRendererObj.FONT_HEIGHT) / 2, new Color(255, 255, 255).getRGB());
-        uiPosition.GLScale(1 / scaleFactor);
+        GlStateManager.scale(1 / scaleFactor, 1 / scaleFactor, 1 / scaleFactor);
+
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
 
@@ -31,19 +31,23 @@ public class GUIManager extends GuiScreen {
 
     @Override
     public void initGui() {
-        for (int i = 0; i < Hephaestus.client.moduleManager.moduleList.size(); i++) {
-            if (Hephaestus.client.moduleManager.moduleList.get(i).name != "ClickGui") {
-                buttonList.add(new GuiButton(i, 2, (12 + mc.fontRendererObj.FONT_HEIGHT * 2) + (20 * i), 119, mc.fontRendererObj.FONT_HEIGHT * 2, Hephaestus.client.moduleManager.moduleList.get(i).name));
+        for (int i = 0; i < getModuleList().size(); i++) {
+            if (getModuleList().get(i).name != "ClickGui") {
+                buttonList.add(new GuiButton(i, x_pos, (12 + mc.fontRendererObj.FONT_HEIGHT * 2) + (20 * i), 119, mc.fontRendererObj.FONT_HEIGHT * 2, getModuleList().get(i).name));
             }
         }
     }
 
     @Override
     protected void actionPerformed(GuiButton button) {
-        for (int i = 0; i < Hephaestus.client.moduleManager.moduleList.size(); i++) {
+        for (int i = 0; i < getModuleList().size(); i++) {
             if (button.id == i) {
-                Hephaestus.client.moduleManager.moduleList.get(i).Toggle();
+                getModuleList().get(i).Toggle();
             }
         }
+    }
+
+    private ArrayList<Module> getModuleList() {
+        return Hephaestus.client.moduleManager.moduleList;
     }
 }
