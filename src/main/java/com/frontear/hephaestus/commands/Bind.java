22c8de6121fd70abfc58dab049f16fa8986d864c
Ali Rizvi <frontear4594@gmail.com>
2018-11-06 20:11:07 -0500

    Fix bind

--- src/main/java/com/frontear/hephaestus/commands/Bind.java
+++ src/main/java/com/frontear/hephaestus/commands/Bind.java
@@ -3,6 +3,7 @@ package com.frontear.hephaestus.commands;
 import com.frontear.hephaestus.Hephaestus;
 import com.frontear.hephaestus.commands.api.Command;
 import com.frontear.hephaestus.modules.api.Module;
+import net.minecraft.client.settings.KeyBinding;
 import org.lwjgl.input.Keyboard;
 
 import java.lang.reflect.Field;
@@ -23,6 +24,7 @@ public class Bind extends Command {
                 try {
                     key = keys.getField("KEY_" + commandArgs[2].toUpperCase());
                     module.module.setKeyCode(key.getInt(null));
+                    KeyBinding.resetKeyBindingArrayAndHash();
                     clientResponse(responseBuilder(module.name + " has been binded to " + Keyboard.getKeyName(module.module.getKeyCode())));
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
